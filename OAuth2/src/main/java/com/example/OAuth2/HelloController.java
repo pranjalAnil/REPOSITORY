57b9f9699ff65403af6fc8a1b56735e62dfa512c
Pranjal.Kunde <pranjalkunde07@gmail.com>
2024-11-04 17:30:44 +0530

    OAuth created by using github

---
+++ OAuth2/src/main/java/com/example/OAuth2/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.OAuth2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String greet(){
+        return "Hello I'm Pranjal";
+    }
+}
