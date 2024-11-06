925b46687f2cc05597c92fd8c6c86113cfd32ef1
Pranjal.Kunde <pranjalkunde07@gmail.com>
2024-11-06 17:21:49 +0530

    _

---
+++ Project_2_Chat_Room/src/main/java/com/example/Project_2_Chat_Room/Project2ChatRoomApplication.java
@@ -0,0 +1,13 @@
+package com.example.Project_2_Chat_Room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2ChatRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2ChatRoomApplication.class, args);
+	}
+
+}
---
+++ Project_2_Chat_Room/src/test/java/com/example/Project_2_Chat_Room/Project2ChatRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Project_2_Chat_Room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project2ChatRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
